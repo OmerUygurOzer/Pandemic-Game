af0fe9b09eaba3cefba84f4ed5dcff77d888a2cf
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 17:16:21 +0000

    copying valuetoname for view

--- pandemic-dopvw/view.h
+++ pandemic-dopvw/view.h
@@ -7,3 +7,12 @@ class PandView
 
 };
 #endif
+
+void valuetoname(int x)//will use this to normalize user interface
+{
+	string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
+							"Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
+							"Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
+							"Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
+	cout << cityname[x];
+};
