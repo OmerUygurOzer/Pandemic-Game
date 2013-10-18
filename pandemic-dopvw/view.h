0403399a5c523327cd0081d5ed1ebd40e5bfd553
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 19:06:38 +0000

    prototypes for screen splash

--- pandemic-dopvw/view.h
+++ pandemic-dopvw/view.h
@@ -1,19 +1,28 @@
 #ifndef VIEW_H
 #define VIEW_H
+#include "Model.h"
 #include <iostream>
 
-class PandView
+class PandView:PandemicMap //not sure if Im inheriting correctly. Will modify as I go.
 {
-
+public:
+	void showCityInfo();
+	void showPlayerInfo();
+	void showNeighbors();
+	void showCubeLocations();
+	void showPlayersLocation();
+	void showInfectionLevel();
+	void showHowManyCardsLeft();
+	void showDiscardedCards();
 
 };
 #endif
 
 void valuetoname(int x)//will use this to normalize user interface
 {
-	string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
+	std::string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
 							"Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
 							"Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
 							"Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
-	cout << cityname[x];
+	std::cout << cityname[x];
 };
