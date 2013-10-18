1db3c76b2ed265af62a6e9e564e078d97cb422bc
woverell@gmail.com <woverell@gmail.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 17:25:19 +0000

    added/modified a few function descriptions in the controller

--- trunk/pandemic-dopvw/controller.h
+++ trunk/pandemic-dopvw/controller.h
@@ -9,17 +9,26 @@ using namespace std;
 class PandController
 {
 public:
+
 	PandController(PandModel m, PandView v);  // initializes the controller
+
+	// * * functions to update view * *
 	void updateView();  // updates the interface
-	void setPlayerTurn(int whoseturn);  // sets new players turn in model
 	int getPlayerTurn();  // returns whose turn it is
 	int getPlayerMoves();  // returns how many turns current player has left
 	int whereIsPlayer();  // returns which city current player is in
-	void setPlayerLocation();  // updates model with where current player is
-	int getCard();  // draws next card from model
+	int getCards();  // gets current players cards to display
+	int getNumCubes(int city);  // returns number of infection cubes on city
+
+	// * * functions to update model * *
+	void setPlayerLocation();  // updates model where player has chosen to go
+	void setPlayerCards();  // updates model what cards are now in players hand
+	void setPlayerMoves();  // updates model on how many moves player has left
+	void setNumCubes(int city);  // updates model on how many cubes are in city
 
 
 private:
+	// no data is stored in the controller
 	PandModel model;
 	PandView view;
 	
