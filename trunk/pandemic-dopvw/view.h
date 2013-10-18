58c8164701d99224aa192951d521cd9e19cb0934
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 21:13:03 +0000

    matched to other files

--- trunk/pandemic-dopvw/view.h
+++ trunk/pandemic-dopvw/view.h
@@ -1,19 +1,10 @@
-
+#ifndef VIEW_H
+#define VIEW_H
 #include "Model.h"
 #include <iostream>
 
-std::string valuetoname(int x)//will use this to normalize user interface
-{
-	std::string temp;
-	std::string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
-							"Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
-							"Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
-							"Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
-	temp = cityname[x];
-	return temp;
-};
 
-class PandView:PandemicMap //not sure if Im inheriting correctly. Will modify as I go.
+class PandView: public PandModel //not sure if Im inheriting correctly. Will modify as I go.
 {
 public:
 	void showCityInfo(int citynum);
@@ -39,12 +30,17 @@ void PandView::showCityInfo(int citynum)
 }
 void PandView::showNeighbors(int citynum)
 {
+	std::string tempcity;
+	std::string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
+							"Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
+							"Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
+							"Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
+	//tempcity = cityname[x];
 	city temp = getCityInfo(citynum);
 	int i = 0;
 	while(temp.adjacentCities[i] != -1)
 	{
-		std::string cityName = valuetoname(temp.adjacentCities[i]);
-		std::cout<<"City: " << cityName <<std::endl;
+		std::cout<<"City: " << cityname[i] <<std::endl;
 		i++;
 	}
 }
@@ -77,3 +73,4 @@ void PandView::showActionMenu(int citynum)
 		break;
 	}
 }
+#endif
