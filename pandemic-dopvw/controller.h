ee9131dc290f0630db55d719cb2904b6c3b0509e
woverell@gmail.com <woverell@gmail.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 20:37:02 +0000

    removed function declarations we don't need yet

--- pandemic-dopvw/controller.h
+++ pandemic-dopvw/controller.h
@@ -14,17 +14,12 @@ public:
 
 	// * * functions to update view * *
 	void updateView();  // updates the interface
-	int getPlayerTurn();  // returns whose turn it is
-	int getPlayerMoves();  // returns how many turns current player has left
 	int whereIsPlayer();  // returns which city current player is in
-	int getCards();  // gets current players cards to display
-	int getNumCubes(int city);  // returns number of infection cubes on city
 
 	// * * functions to update model * *
 	void setPlayerLocation();  // updates model where player has chosen to go
-	void setPlayerCards();  // updates model what cards are now in players hand
 	void setPlayerMoves();  // updates model on how many moves player has left
-	void setNumCubes(int city);  // updates model on how many cubes are in city
+	void NextTurn();  // ends current players turn and starts next players turn
 
 
 private:
