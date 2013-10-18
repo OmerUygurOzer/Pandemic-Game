5494fb4bee9ae96d5f85d08705a91e6d2b9052f8
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-18 21:20:31 +0000

    added a few more tests

--- pandemic-dopvw/controller.cpp
+++ pandemic-dopvw/controller.cpp
@@ -31,5 +31,25 @@ int main()
 	PandModel GameInstance;
 	PandView Screens;
 	Screens.showCityInfo(1);
+	Screens.showNeighbors(1);
+	Screens.showActionMenu(1);
+	int answer;
+	std::cin>>answer;//modify to restrict input from 1-6
+	//use answer to bring up other menu items.
+	switch (answer)
+	{
+	case 1: Screens.showNeighbors(1);//and move to neighbor
+		break;
+	case 2: //use citycard
+		break;
+	case 3://remove cube
+		break;
+	case 4: //build research
+		break;
+	case 5: //show infected cities
+		break;
+	case 6://misc
+		break;
+	}
 	return 0;
 }
