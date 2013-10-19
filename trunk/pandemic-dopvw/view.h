4176c7f4d27ceb9fe4a6fd9da6e0d3e8cdc0a317
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-10-19 00:00:08 +0000

    Fixed to work with controller.cpp

--- trunk/pandemic-dopvw/view.h
+++ trunk/pandemic-dopvw/view.h
@@ -6,10 +6,15 @@
 
 class PandView: public PandModel //not sure if Im inheriting correctly. Will modify as I go.
 {
+	PandModel model;
 public:
+	PandView();
+	PandView(PandModel m);
+	
+	
 	void showCityInfo(int citynum);
 	void showActionMenu(int citynum);
-	void showPlayerInfo();
+	void showPlayerInfo(int playernum);
 	void showNeighbors(int citynum);
 	void showCubeLocations();
 	void showPlayersLocation();
@@ -18,6 +23,17 @@ public:
 	void showDiscardedCards();
 
 };
+
+PandView::PandView()
+{
+
+}
+
+PandView::PandView(PandModel m)
+{
+	model = m;
+}
+
 
 void PandView::showCityInfo(int citynum)
 {
@@ -40,13 +56,14 @@ void PandView::showNeighbors(int citynum)
 	int i = 0;
 	while(temp.adjacentCities[i] != -1)
 	{
-		std::cout<<"City: " << cityname[temp.adjacentCities[i]] <<std::endl;
+		std::cout<<i+1<<". City: " << cityname[temp.adjacentCities[i]] <<std::endl;
 		i++;
 	}
 }
 
 void PandView::showActionMenu(int citynum)
 {
+	std::cout<<std::endl;
 	std::cout<<"1. move to adjacent" << std::endl;
 	std::cout<<"2. use city card " << std::endl;
 	std::cout<<"3. remove cube " << std::endl;
@@ -56,4 +73,20 @@ void PandView::showActionMenu(int citynum)
 	std::cout<<"What would you like to do : " << std::endl;
 	
 }
+
+void PandView::showPlayerInfo(int playernum)
+{
+	std::string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
+							"Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
+							"Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
+							"Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
+	Playerchar temp = model.getPlayerInfo(playernum-1);
+	std::cout<<"Player name: " <<  temp.playerName << std::endl;
+	std::cout<<"Player location: " << cityname[temp.location] << std::endl;
+	//std::cout<<"Player role: " << temp.playerName << std::endl;
+	//::cout<<"Player cards: " << temp.playerName << std::endl;
+}
+
+
+
 #endif
