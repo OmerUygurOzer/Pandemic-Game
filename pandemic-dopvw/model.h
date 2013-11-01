dceee7c8a88d34b06997ea2bcf5d8cc7c41f3ad1
grafiks8@aol.com <grafiks8@aol.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-11-01 21:20:46 +0000

    fixed citycolor in constructor

--- pandemic-dopvw/model.h
+++ pandemic-dopvw/model.h
@@ -84,10 +84,11 @@ public:
 
 PandModel::PandModel()//constructor
 {
-	char citycolors[48]= {"G", "B", "G", "R", "R", "Y", "Y", "G", "G", "B", "G", "B", "R",
-                            "R", "G", "R", "Y", "G", "Y", "Y", "G", "Y", "Y", "B", "Y", "B",
-                            "R", "Y", "Y", "B", "G", "G", "B", "R", "B", "G", "B", "Y",
-                            "Y", "R", "R", "B", "R", "R", "G", "R", "B", "B"};
+	char citycolors[48]= {'G', 'B', 'G', 'R', 'R', 'Y', 'Y', 'G', 'G', 'B',
+							'G', 'B', 'R', 'R', 'G', 'R', 'Y', 'G', 'Y', 'Y',
+                            'G', 'Y', 'Y', 'B', 'Y', 'B', 'R', 'Y', 'Y', 'B',
+                            'G', 'G', 'B', 'R', 'B', 'G', 'B', 'Y', 'Y', 'R',
+                            'R', 'B', 'R', 'R', 'G', 'R', 'B', 'B'};
 	 std::string cityname[48] = {"Algiers", "Atlanta", "Baghdad", "Bangkok", "Beijing", "Bogota", "Buenos Aires", "Cairo", "Chennai", "Chicago", "Delhi", "Essen", "Ho Chi Minh City",
                             "Hong Kong", "Istanbul", "Jakarta", "Johannesburg", "Karachi", "Khartoum", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Madrid",
                             "Manila", "Mexico City", "Miami", "Milan", "Moscow", "Mumbai", "New York", "Osaka", "Paris", "Riyadh", "San Francisco", "Santiago",
