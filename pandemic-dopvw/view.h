4160a72cc57f437fa015a6d138587f6be1bad013
omer.u.ozer@gmail.com <omer.u.ozer@gmail.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-11-02 02:52:22 +0000

    added more functionality to the game. Role actions are now being managed in a more detailed manner. Each player is randomly assigned a role as we discussed in our meeting. I left comments on most of the parts I changed/added.

--- pandemic-dopvw/view.h
+++ pandemic-dopvw/view.h
@@ -13,7 +13,7 @@ public:
 	
 	void showIntro();
 	void showCityInfo(int citynum);
-	void showActionMenu();
+	void showActionMenu(int p);
 	void showPlayerInfo(int playernum);
 	void showNeighbors(int citynum);
 	void showCubeLocations();//
@@ -21,7 +21,8 @@ public:
 	void showInfectionLevel();//stub
 	void showHowManyCardsLeft();//stub
 	void showDiscardedCards();//stub
-	void listActions(int player);//
+	void listDescriptions(int role);//lists the descriptons of roles
+	void listActions(int role);//lists specific role actions
 };
 
 PandView::PandView()
@@ -66,7 +67,7 @@ void PandView::showNeighbors(int citynum)
 	}
 }
 
-void PandView::showActionMenu()
+void PandView::showActionMenu(int p)
 {
 	std::cout<<std::endl;
 	std::cout<<"1. move to adjacent" << std::endl;
@@ -75,6 +76,7 @@ void PandView::showActionMenu()
 	std::cout<<"4. build research center " << std::endl;
 	std::cout<<"5. list infected cities " << std::endl;
 	std::cout<<"6. list other things " << std::endl;//expansion for later
+	listActions(p);
 	std::cout<<"What would you like to do : ";
 	
 }
@@ -88,8 +90,14 @@ void PandView::showPlayerInfo(int playernum)
 	Playerchar temp = model.getPlayerInfo(playernum-1);
 	std::cout<<"Player name: " <<  temp.playerName << std::endl;
 	std::cout<<"Player location: " << cityname[temp.location] << std::endl;
-	//std::cout<<"Player role: " << temp.playerName << std::endl;
+	std::cout << "Player role: " << temp.profName << std::endl;
+	//std::cout << "Player role(int): " << temp.profession << std::endl; //Added to help with debug 
+	std::cout << "Role details: " << std::endl;
+	listDescriptions(temp.profession);
+
+    //std::cout<<"Player role: " << temp.playerName << std::endl;
 	//::cout<<"Player cards: " << temp.playerName << std::endl;
+	
 }
 
 void PandView::showCubeLocations()
@@ -131,42 +139,83 @@ void PandView::showDiscardedCards()
 	std::cout<<"This function will show the cards that have been discarded \n";
 }
 
-void PandView::listActions(int player)
+void PandView::listActions(int role){
+	if (role == 0){
+		std::cout << "7. Store a discarded card" << std::endl;
+		
+	}
+	if (role == 1){
+		std::cout << "7. Move a pawn of your own" << std::endl;
+		std::cout << "8. Move a pawn of another player" << std::endl;
+		
+	}
+	if (role == 2){
+		std::cout << "Passive abilities:" << std::endl;
+	    std::cout << "					* When a city is treated, all cubes of the same color are treated " << std::endl;
+		std::cout << "					* Known cures are administered with no action cost " << std::endl;
+		
+
+	}
+	if (role == 3){
+		std::cout << "7. Build a research station" << std::endl;
+		
+
+	}
+	if (role == 4){
+		std::cout << "Passive abilities:" << std::endl;
+		std::cout << "					* Prevents disease cube placemenets in the city and the ones surrounding " << std::endl;
+	
+
+	}
+	if (role == 5){
+		std::cout << "7. Give one of your cards to another player" << std::endl;
+		
+	}
+	if (role == 6){
+		std::cout << "Passive abilities:" << std::endl;
+		std::cout << "					* Needs only 4 cards of the same color to discover a cure " << std::endl;
+	}
+
+}
+
+void PandView::listDescriptions(int role)
 {
-	Playerchar temp = getPlayerInfo(player);
+	//Playerchar temp = getPlayerInfo(player);
+
 	std::cout <<std::endl;
-	if (temp.profession==0){
+	if (role==0){
 		std::cout <<"A player who has the 'Contigency Plan' role can take any discarded Event card"<<std::endl;
 		std::cout <<"and store it with this card."<<std::endl;
 		std::cout <<"The stored Event card will be discarded once used!"<<std::endl;
 	}
-	if (temp.profession==1){
+	if (role == 1){
 		std::cout <<"A player who has the 'Dispatcher' role can move their fellow player pawns on your"<<std::endl;
 		std::cout <<"turns as if they were their own."<<std::endl;
 		std::cout <<"They can also move any pawn to another city containing a pawn for 1 action."<<std::endl;
 	}
-	if (temp.profession==2){
+	if (role == 2){
 		std::cout <<"A player who has the 'Medic' role can remove all cubes of a single color when they treat a city."<<std::endl;
 		std::cout <<"They can administer known cures for free."<<std::endl;
 	
 	}
-	if (temp.profession==3){
+	if (role == 3){
 		std::cout <<"A player who has the 'Operations Expert' can build a research station in their current city for"<<std::endl;
 		std::cout <<"one action."<<std::endl;
 		
 	}
-	if (temp.profession==4){
+	if (role == 4){
 		std::cout <<"A player who has the 'Quarantine Specialist' role can prevent disease cube placemenets in the city"<<std::endl;
 		std::cout <<"they are in and all of the ones connected to it"<<std::endl;
 		
 	}
-	if (temp.profession==5){
+	if (role == 5){
 		std::cout <<"A player who has the 'Researcher' role can give a player cards from their hands for 1 action per card."<<std::endl;
 		std::cout <<"Both of their pawns must be in the same city but it doesn't matter which city they are in."<<std::endl;
 	}
-	if (temp.profession==6){
+	if (role == 6){
 		std::cout <<"A player who has the 'Scientist' role need only 4 cards of the same color to discover a cure."<<std::endl;
 	}
 
+	std::cout << std::endl;
 };
 #endif
