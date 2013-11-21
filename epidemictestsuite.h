d8e94f4bdbfe2a546f7237a5e998d2b2d5c0b894
woverell@gmail.com <woverell@gmail.com@b6b7a1df-e875-4423-95d1-aab59dc0d661>
2013-11-21 23:35:22 +0000

    Added function testCardPositions to check position of each card after merging decks

--- epidemictestsuite.h
+++ epidemictestsuite.h
@@ -31,21 +31,21 @@ public:
                             "Sao Paulo", "Seoul", "Shanghai", "St. Petersburg", "Sydney", "Taipei", "Tehran", "Tokyo", "Toronto", "Washington"};
 
 		infectionCard infectCard;
-		for(int i = 0; i<30; i++)  //fill draw deck with first 30 cards
+		for(int i = 0; i<30; i++)  //fill draw deck with first 30 cards (top of deck is city 29 bottom is city 0)
 		{
 			infectCard.cardType = "City";
 			infectCard.cardDescription = cityname[i];
 			infectCard.city = i;
 			infectCard.color = 0;  // color not important
-			infectionDeck.push_back(infectCard);//add onto infectiondeck after info populated 
+			infectionDeck.push_front(infectCard);//add onto infectiondeck after info populated
 		}
-		for(int i = 30; i<48; i++)  // fill discard deck with final 18 cards
+		for(int i = 30; i<48; i++)  // fill discard deck with final 18 cards (top of deck is city 47, bottom is city 30)
 		{
 			infectCard.cardType = "City";
 			infectCard.cardDescription = cityname[i];
 			infectCard.city = i;
 			infectCard.color = 0;  // color not important
-			discardInfectionDeck.push_back(infectCard);//add onto infectiondeck after info populated 
+			discardInfectionDeck.push_front(infectCard);//add onto infectiondeck after info populated 
 		}
 
 	}
@@ -56,7 +56,7 @@ public:
 		TS_ASSERT_EQUALS(infectionDeck.size(), 30); // before merge should have 30 cards in draw deck
 		TS_ASSERT_EQUALS(discardInfectionDeck.size(), 18); // before merge should have 18 cards in discard deck
 		mergeInfectionDecks(discardInfectionDeck, infectionDeck); // puts discardInfectdionDeck on top of infectionDeck
-        TS_ASSERT_EQUALS(infectionDeck.size(), 48); // after merge draw deck should have all 48 cards
+		TS_ASSERT_EQUALS(infectionDeck.size(), 48); // after merge draw deck should have all 48 cards
 		TS_ASSERT_EQUALS(discardInfectionDeck.size(), 0); // after merge discard deck should be empty
     }
 
@@ -67,11 +67,19 @@ public:
 		testCard1 = discardInfectionDeck.front(); // set testCard1 as card on top of discard deck
 		mergeInfectionDecks(discardInfectionDeck, infectionDeck); // puts discardInfectdionDeck on top of infectionDeck
 		testCard2 = infectionDeck.front(); // set testCard2 as card on top of merged infectionDeck
-		TS_ASSERT_EQUALS(testCard1.city, testCard2.city);
+		TS_ASSERT_EQUALS(testCard1.city, testCard2.city); // top of discard should now be on top of draw deck
 	}
 
 	void testCardPositions(void) // more complete test to test that all cards are in correct position
 	{
-
+		infectionCard testCard; // card to test with
+		fillTestDecks(); // populate the decks
+		mergeInfectionDecks(discardInfectionDeck, infectionDeck); // puts discardInfectdionDeck on top of infectionDeck
+		for(int i = 0; i<48; i++) // go through cards of draw deck
+		{
+			testCard = infectionDeck.back(); // set as bottom card
+			infectionDeck.pop_back(); // remove bottom card
+			TS_ASSERT_EQUALS(testCard.city, i); // should be equal to i
+		}
 	}
 };
